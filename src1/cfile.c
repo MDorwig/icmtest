817fd03302d6c9509c9e431f8c8493e480c33214
Martin Dorwig <martin.dorwig@gmail.com>
2015-10-21 22:47:39 +0200

    GetSource überarbeitet mit COMMON_FILES , config_section und host_section
    bin directory-struktur geändert,
    nur noch eine unterverzeichnis per configuration

--- src1/cfile.c
+++ src1/cfile.c
@@ -6,6 +6,10 @@
 
 int main(int argc,char * argv[])
 {
-	printf("Hello World data_item1=%d data_item2=%d\n",data_item1,data_item2);
+  printf("Hello World\""
+        "Version %s\n",GIT_VERSION);
+	printf("data_item1=%d\n"
+	       "data_item2=%d\n",
+	       data_item1,data_item2);
 	return 0;
 }
